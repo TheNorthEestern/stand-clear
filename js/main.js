fb46215d21c64f5bb0b098f85e126a0f7d226894
Kacy James <inquen@gmail.com>
2013-01-02 15:28:17 -0500

    Removed main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('.door-controller').click(function(){
-    $('.car-door').toggleClass('doors-open');
-    if (!$.find('.doors-open').length > 0)
-     $('.door-closure-lamp').toggleClass('door-closure-lamp-on');
-  });
-});
