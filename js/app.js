19c1d12f3e3a61f36e430307ee64972ab6071c67
Kacy James <inquen@gmail.com>
2013-01-02 17:42:17 -0500

    Added number of door opens/closes

--- js/app.js
+++ js/app.js
@@ -1,28 +1,35 @@
-StandClear = Ember.Application.create();
+var StandClear = Ember.Application.create();
+
+StandClear.doors = Ember.Object.create({
+    doorsOpen: false,
+    opens:0,
+    closes: 0
+});
 
 StandClear.doorController = Ember.Controller.create({
-  doorsOpen: true,
-  toggleDoors:function(e){
-   if(!this.get('doorsOpen')) {
-     this.set('doorsOpen', true); 
-   }	
-   else{
-     this.set('doorsOpen', false)	
-   }
-  }
+    toggleDoors:function(e){
+         if(!StandClear.get('doors.doorsOpen')) {
+            StandClear.set('doors.doorsOpen', true);
+            StandClear.doors.incrementProperty('opens');
+            console.log(StandClear.get('doors.opens'));  
+         }
+         else{
+            StandClear.set('doors.doorsOpen', false);
+            StandClear.doors.incrementProperty('closes');
+         }
+       }
 });
 
 StandClear.DoorView = Ember.View.extend({
-  tagName: 'div',
-  classNames:['car-door'],
-  classNameBindings: 'StandClear.doorController.doorsOpen',
-})
-
-StandClear.DoorStateView = Ember.View.extend({
-  tagName: 'button',
-  classNames:['door-controller'],
-  click:function(e){
-    StandClear.doorController.toggleDoors();
-  }
+    tagName: 'div',
+    classNames:['car-door'],
+    classNameBindings: 'StandClear.doors.doorsOpen'
 });
 
+StandClear.DoorStateView = Ember.View.extend({
+    tagName: 'button',
+    classNames:['door-controller'],
+    click: function(e){
+      StandClear.doorController.toggleDoors();
+    }
+});
