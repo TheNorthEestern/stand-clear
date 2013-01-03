4ea4aac825510a6bbc85475f9d1394c6a6bf2ffd
Kacy James <inquen@gmail.com>
2013-01-02 20:05:59 -0500

    Added counters and door signals, although the arent 100% up to snuff

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var StandClear = Ember.Application.create();
 
 StandClear.doors = Ember.Object.create({
-    doorsOpen: false,
+    doorsOpen: true,
+    lastAction: "",
     opens:0,
     closes: 0
 });
@@ -10,11 +11,12 @@ StandClear.doorController = Ember.Controller.create({
     toggleDoors:function(e){
          if(!StandClear.get('doors.doorsOpen')) {
             StandClear.set('doors.doorsOpen', true);
+            StandClear.set('doors.lastAction', 'open');
             StandClear.doors.incrementProperty('opens');
-            console.log(StandClear.get('doors.opens'));  
          }
          else{
             StandClear.set('doors.doorsOpen', false);
+            StandClear.set('doors.lastAction', 'closed');
             StandClear.doors.incrementProperty('closes');
          }
        }
@@ -25,6 +27,18 @@ StandClear.DoorView = Ember.View.extend({
     classNames:['car-door'],
     classNameBindings: 'StandClear.doors.doorsOpen'
 });
+
+StandClear.DoorClosureLampView = Ember.View.extend({
+    tagName: 'div',
+    classNames:['door-lamp', 'closure-lamp'],
+    classNameBindings: 'StandClear.doors.doorsOpen::door-closure-lamp-on',
+});
+
+StandClear.DoorOpenLampView = Ember.View.extend({
+  tagName: 'div',
+  classNames:['door-lamp', 'open-lamp'],
+  classNameBindings: 'StandClear.doors.doorsOpen:door-open-lamp-on'
+})
 
 StandClear.DoorStateView = Ember.View.extend({
     tagName: 'button',
